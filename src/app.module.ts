13fe1db581a47c3940b6f64ac312fe0f9164a440
husan <abdigafurovhusan@gmail.com>
2025-06-03 00:32:53 +0500

    added port mapping from .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,13 @@ import { FavoritesModule } from './favorites/favorites.module';
 import { TrackModule } from './track/track.module';
 import { AlbumModule } from './album/album.module';
 import { ArtistModule } from './artist/artist.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes ConfigService available globally
+    }),
     UserModule,
     ArtistModule,
     AlbumModule,
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') || 4000;
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -12,6 +16,6 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  await app.listen(4000);
+  await app.listen(port);
 }
 bootstrap();
