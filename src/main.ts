662afde19b4fc5969527a98986b04b695492104d
husabdigafurov <husabdigafurov@team.amocrm.com>
2025-06-16 20:29:36 +0500

    init logger

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { MyLogger } from './logger/logger.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || 4000;
@@ -16,6 +19,9 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  app.useLogger(app.get(MyLogger));
+
   await app.listen(port);
 }
 bootstrap();
